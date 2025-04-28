ce0992f03f30c7cb67414f493c19d8f007b77428
Badawy24 <abdelrahman.ahmed2410@gmail.com>
2025-04-29 02:11:45 +0300

    Lab3 Javascript

---
+++ lab3/consumingScript.js
@@ -0,0 +1,7 @@
+console.log(stringPascalCase("the quick brown fox"));
+
+console.log(longestWord("Web Development Tutorial"));
+
+console.log(alphabeticalOrder("javascript"));
+
+console.log(commonElements([1, 2, 3],[2, 3, 4]));
---
+++ lab3/functionsScript.js
@@ -0,0 +1,51 @@
+// -------- 1 --------
+function stringPascalCase(str) {
+
+    let strToArray = str.split(' ');
+    let mapArray = strToArray.map((wordFirstUpper) => {
+        return wordFirstUpper.charAt(0).toUpperCase() + wordFirstUpper.slice(1).toLowerCase();
+    });
+    return mapArray.join(' ')
+}
+
+// -------- 2 --------
+
+function longestWord(str) {
+    let strToArray = str.split(' ');
+
+    let lengthOfWords = strToArray.map((word) => {
+        return (word.length);
+    });
+    let max = 0;
+    let targetIndex = 0;
+    for (let i = 0; i < lengthOfWords.length; i++) {
+        if (lengthOfWords[i] > max) {
+            max = lengthOfWords[i];
+            targetIndex = lengthOfWords.indexOf(lengthOfWords[i])
+        }
+    }
+    return strToArray[targetIndex];
+}
+
+// -------- 3 --------
+
+function alphabeticalOrder(str) {
+    let strToArray = str.split('');
+    return strToArray.sort().join('');
+}
+
+// -------- 4 --------
+function commonElements(arr1, arr2) {
+        let resArray = [];
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr2.includes(arr1[i])) {
+                resArray.push(arr1[i]);
+            }
+        }
+    
+        return resArray;
+    
+}
+
+
+// -------- 5 --------
